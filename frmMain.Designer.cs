40769122e5f0dd109045ef1a5d5c4e29a6d8d5e9
Jitnaught <letsplayordy@gmail.com>
2016-12-18 15:42:16 -0600

    Fixed NotifyIcon being shown before form is minimized.

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -290,7 +290,6 @@
             this.notifyIcon1.BalloonTipTitle = "JN Soundboard";
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "JN Soundboard";
-            this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
             // 
             // frmMain
